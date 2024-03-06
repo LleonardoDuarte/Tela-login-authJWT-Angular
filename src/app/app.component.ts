8668ba0f71d8c6bb4891f58f3cf265356c44fd68
LeonardoDuarte <leopd_00@hotmail.com>
2024-03-06 09:18:06 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ` <h1>teste</h1>
+    <router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  title = 'projeto-pratico2-TelaLogin';
-}
+export class AppComponent {}
